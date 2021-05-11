566fb7f5ca3ab66d11ba9efe28491204df357d98
miki hayden <miki.palao.palmer@gmail.com>
2021-05-11 21:29:31 +0200

    get day tests created

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -1,5 +1,7 @@
 package training.weather;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -13,5 +15,32 @@ public class WeatherForecastTest {
 		WeatherForecast weatherForecast = new WeatherForecast();
 		String forecast = weatherForecast.getCityWeather("Madrid", new Date());
 		System.out.println(forecast);
+	}
+
+	@Test
+	public void givenNullDateShouldReturnANewDate() {
+		WeatherForecast weatherForecast = new WeatherForecast();
+
+		 /*------------------- When --------------------- */
+			LocalDate date  = weatherForecast.getDate();
+			LocalDate dateToCompare = LocalDate.now();
+
+			
+		 /*------------------- Then  --------------------- */
+			assertEquals(dateToCompare.toString(), date.toString());
+	}
+
+
+	@Test
+	public void givenDateShouldReturnTheDate() {
+		WeatherForecast weatherForecast = new WeatherForecast();
+
+		 /*------------------- When --------------------- */
+			LocalDate date  = weatherForecast.getDate(new LocalDate.of(2021,05,10));
+			LocalDate dateToCompare = LocalDate.of(2021,05,10);
+
+			
+		 /*------------------- Then  --------------------- */
+			assertEquals(dateToCompare.toString(), date.toString());
 	}
 }
