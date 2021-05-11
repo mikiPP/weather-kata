d2291945241a30f3ac0cff115c2d8ceffeb374a3
miki hayden <miki.palao.palmer@gmail.com>
2021-05-12 00:07:31 +0200

    Added the new functions to the code and some values extacted to class properties.

--- src/main/java/training/weather/WeatherForecast.java
+++ src/main/java/training/weather/WeatherForecast.java
@@ -5,37 +5,45 @@ import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class WeatherForecast {
 
+	private final long DAYS = 6;
+	private String format = "yyyy-MM-dd";
+	private String path = "https://www.metaweather.com/api/location/";
+
 	public String getCityWeather(String city, LocalDate datetime) throws IOException {
-		// LocalDate date = getDate(datetime); 
+		LocalDate date = getDate(datetime); 
+		String weather = "";
 
+		if (!isDateBeforeNextDays(date, DAYS)){
+			return "";
+		}
+	
+		HttpRequestFactory rf = new NetHttpTransport().createRequestFactory();
+		HttpRequest req = rf
+			.buildGetRequest(new GenericUrl(this.path + "search/?query=" + city));
+		String r = req.execute().parseAsString();
+		JSONArray array = new JSONArray(r);
+		String woe = array.getJSONObject(0).get("woeid").toString();
+		rf = new NetHttpTransport().createRequestFactory();
+		req = rf.buildGetRequest(new GenericUrl(this.path + woe));
+		r = req.execute().parseAsString();
+		JSONArray results = new JSONObject(r).getJSONArray("consolidated_weather");
+		for (int i = 0; i < results.length(); i++) {
+			if (date.format(DateTimeFormatter.ofPattern(this.format))
+				.equals(results.getJSONObject(i).get("applicable_date").toString())) {
+				weather =results.getJSONObject(i).get("weather_state_name").toString();
+				break;
+			}
+		}
 
-		// if (datetime.before(new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 6)))) {
-		// 	HttpRequestFactory rf = new NetHttpTransport().createRequestFactory();
-		// 	HttpRequest req = rf
-		// 		.buildGetRequest(new GenericUrl("https://www.metaweather.com/api/location/search/?query=" + city));
-		// 	String r = req.execute().parseAsString();
-		// 	JSONArray array = new JSONArray(r);
-		// 	String woe = array.getJSONObject(0).get("woeid").toString();
-		// 	rf = new NetHttpTransport().createRequestFactory();
-		// 	req = rf.buildGetRequest(new GenericUrl("https://www.metaweather.com/api/location/" + woe));
-		// 	r = req.execute().parseAsString();
-		// 	JSONArray results = new JSONObject(r).getJSONArray("consolidated_weather");
-		// 	for (int i = 0; i < results.length(); i++) {
-		// 		if (new SimpleDateFormat("yyyy-MM-dd").format(datetime)
-		// 			.equals(results.getJSONObject(i).get("applicable_date").toString())) {
-		// 			return results.getJSONObject(i).get("weather_state_name").toString();
-		// 		}
-		// 	}
-		// }
-		return "";
+		return weather;
 	}
 
 	/**
