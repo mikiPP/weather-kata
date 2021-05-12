3b98729524f42277ab02c6deb62e2c4e912ce347
miki hayden <miki.palao.palmer@gmail.com>
2021-05-12 19:39:36 +0200

    variables renamed

--- src/main/java/training/weather/WeatherForecast.java
+++ src/main/java/training/weather/WeatherForecast.java
@@ -15,28 +15,27 @@ import training.time.Time;
 
 public class WeatherForecast {
 
-	private final long DAYS = 6;
-	private String format = "yyyy-MM-dd";
+	private long days = 6;
 	private String path = "https://www.metaweather.com/api/location/";
+	private String format = "yyyy-MM-dd";
 
 	public String getCityWeather(String city, LocalDate datetime) throws IOException {
 		LocalDate date = Time.getDate(datetime); 
 		String weather = "";
 
-		if (!Time.isDateBeforeNextDays(date, DAYS)){
+		if (!Time.isDateBeforeNextDays(date, days)){
 			return "";
 		}
 	
-		HttpRequestFactory rf = new NetHttpTransport().createRequestFactory();
-		HttpRequest req = rf
+		HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+		HttpRequest request = requestFactory
 			.buildGetRequest(new GenericUrl(this.path + "search/?query=" + city));
-		String r = req.execute().parseAsString();
-		JSONArray array = new JSONArray(r);
+		String response = request.execute().parseAsString();
+		JSONArray array = new JSONArray(response);
 		String woe = array.getJSONObject(0).get("woeid").toString();
-		rf = new NetHttpTransport().createRequestFactory();
-		req = rf.buildGetRequest(new GenericUrl(this.path + woe));
-		r = req.execute().parseAsString();
-		JSONArray results = new JSONObject(r).getJSONArray("consolidated_weather");
+		request = requestFactory.buildGetRequest(new GenericUrl(this.path + woe));
+		response = request.execute().parseAsString();
+		JSONArray results = new JSONObject(response).getJSONArray("consolidated_weather");
 		for (int i = 0; i < results.length(); i++) {
 			if (date.format(DateTimeFormatter.ofPattern(this.format))
 				.equals(results.getJSONObject(i).get("applicable_date").toString())) {
@@ -44,6 +43,7 @@ public class WeatherForecast {
 				break;
 			}
 		}
+		// return results.stream
 
 		return weather;
 	}
